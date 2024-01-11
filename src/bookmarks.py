4fed01d51176364855daf6168e228bc1d7573161
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-11 21:23:10 +0200

    Get single bookmark

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -60,4 +60,23 @@ def handle_bookmarks():
 
         }
         return jsonify({'data': data, 'meta': meta}), HTTP_200_OK
+    
+
+@bookmarks.get("/<int:id>")
+@jwt_required()
+def get_bookmark(id):
+    current_user = get_jwt_identity()
+    bookmark = Bookmark.query.filter_by(user_id=current_user, id=id).first()
+
+    if not bookmark:
+        return jsonify({"message": "bookmark not found"}), HTTP_404_NOT_FOUND
+    return jsonify({
+        "id": bookmark.id,
+        "url": bookmark.url,
+        "short_url": bookmark.short_url,
+        "visit": bookmark.visits,
+        "body": bookmark.body,
+        "created_at": bookmark.created_at,
+        "updated_at": bookmark.updated_at
+    }), HTTP_200_OK
         
