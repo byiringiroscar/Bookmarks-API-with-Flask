9c8d50d8bec6f376e4a1d0555edaf52dd4336865
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-11 11:43:30 +0200

    wip on setup

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import os
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     if test_config is None:
         app.config.from_mapping(
-            SECRET_KEY='dev',
+            SECRET_KEY=os.environ.get("SECRET_KEY"),
         )
     else:
         app.config.from_mapping(test_config)
