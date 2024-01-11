2d2428a86f6f065aa23d62d94331fa6fadceaaba
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-11 14:37:20 +0200

    wip

--- .flaskenv
+++ .flaskenv
@@ -1,3 +1,4 @@
 export FLASK_ENV=development
 export FLASK_APP=src
 export SQLALCHEMY_DB_URI=sqlite:///bookmarks.db
+export DEBUG=True
--- src/__init__.py
+++ src/__init__.py
@@ -5,6 +5,7 @@ from src.auth import auth
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+    app.config['DEBUG'] = True
     if test_config is None:
         app.config.from_mapping(
             SECRET_KEY=os.environ.get("SECRET_KEY"),
@@ -12,7 +13,6 @@ def create_app(test_config=None):
         )
     else:
         app.config.from_mapping(test_config)
-
     db.app=app
     db.init_app(app)
     with app.app_context():
