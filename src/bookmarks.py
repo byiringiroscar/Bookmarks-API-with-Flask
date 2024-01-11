a36d3af8ea23aa95d04b9a94523e2176773f271c
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-11 20:24:22 +0200

    Add bookmark

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -8,8 +8,29 @@ from src.database import Bookmark, db
 bookmarks = Blueprint("bookmarks", __name__, url_prefix="/api/v1/bookmarks")
 
 
-@bookmarks.get("/all")
+@bookmarks.route('/', methods=['POST', 'GET'])
+@jwt_required()
 def get_bookmarks():
-    return jsonify({
-        'message': "book marks ok"
-    }), HTTP_200_OK
+    current_user = get_jwt_identity()
+    if request.method == 'POST':
+        body=request.get_json().get('body', '')
+        url=request.get_json().get('url', '')
+
+        if not validators.url(url):
+            return jsonify({"error": "url is not valid"}), HTTP_400_BAD_REQUEST
+        
+        if Bookmark.query.filter_by(url=url).first():
+            return jsonify({"error": "url already exists"}), HTTP_409_CONFLICT
+        bookmark = Bookmark(url=url, body=body, user_id=current_user)
+        db.session.add(bookmark)
+        db.session.commit()
+        return jsonify({
+            "id": bookmark.id,
+            "url": bookmark.url,
+            "short_url": bookmark.short_url,
+            "visit": bookmark.visits,
+            "body": bookmark.body,
+            "created_at": bookmark.created_at,
+            "updated_at": bookmark.updated_at
+        }), HTTP_201_CREATED
+        
