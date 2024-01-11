0981a5e0683f42c3080252e0549ee995a23db47d
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-11 12:21:48 +0200

    Add short url functionality

--- src/database.py
+++ src/database.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import string
+import random
 
 
 db = SQLAlchemy()
@@ -29,9 +31,19 @@ class Bookmark(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.now())
     updated_at = db.Column(db.DateTime, onupdate=datetime.now())
 
+    def generate_short_characters(self):
+        characters = string.digits + string.ascii_letters
+        picked_chars = ''.join(random.choices(characters, k=3))
+
+        link=self.query.filter_by(short_url=picked_chars).first()
+        if link:
+            self.generate_short_characters()
+        else:
+            return picked_chars
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.short_url = self.generate_short_url()
+        self.short_url = self.generate_short_characters()
 
 
     def __repr__(self) -> str:
