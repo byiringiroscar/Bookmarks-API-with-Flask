d03d9783ace476c8337a475e4cfa06ee730008ab
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-11 12:23:52 +0200

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -6,6 +6,7 @@ def create_app(test_config=None):
     if test_config is None:
         app.config.from_mapping(
             SECRET_KEY=os.environ.get("SECRET_KEY"),
+            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI"),
         )
     else:
         app.config.from_mapping(test_config)
