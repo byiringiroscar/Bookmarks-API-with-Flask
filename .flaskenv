4107d5db5d987288df42f0d6c78b43358c49212d
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-11 15:29:52 +0200

    Add setup for jwt

--- .flaskenv
+++ .flaskenv
@@ -2,3 +2,4 @@ export FLASK_ENV=development
 export FLASK_APP=src
 export SQLALCHEMY_DB_URI=sqlite:///bookmarks.db
 export DEBUG=True
+export JWT_SECRET_KEY='jdjnsdjsndkdsnjsdwyweiuweuweknsd'
--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 import os
 from src.database import db
 from src.auth import auth
+from flask_jwt_extended import JWTManager
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -11,11 +12,15 @@ def create_app(test_config=None):
             SECRET_KEY=os.environ.get("SECRET_KEY"),
             SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI"),
             SQLALCHEMY_TRACK_MODIFICATIONS=False,
+            JWT_SECRET_KEY=os.environ.get("JWT_SECRET_KEY"),
         )
     else:
         app.config.from_mapping(test_config)
     db.app=app
     db.init_app(app)
+
+    JWTManager(app)
+
     with app.app_context():
         db.create_all()  # Create tables only if they don't exist
     app.register_blueprint(auth)
