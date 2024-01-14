76890f2844d5a7b2207426f33a34d1b38fd5f297
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-14 13:03:19 +0200

    wip

--- src/__init__.py
+++ src/__init__.py
@@ -8,7 +8,6 @@ from src.constants.http_status_code import *
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    app.config['DEBUG'] = True 
     if test_config is None:
         app.config.from_mapping(
             SECRET_KEY=os.environ.get("SECRET_KEY"),
